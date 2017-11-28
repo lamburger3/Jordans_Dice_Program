aaa09736ae785d0b066039c4bec58c019e3a791c
lamburger3 <lamburger3@gmail.com>
2017-11-27 22:05:38 -0800

    Fixed Dice System with Except

--- dicerollsystem.py
+++ dicerollsystem.py
@@ -1,18 +1,20 @@
 import random
 import time
 
-def roll():
-  mod = input("What is your modifier for this roll?: ")
-  random_roll = (random.randint(1,20))
-  sum = int(mod) + int(random_roll)
-  print("Your true roll is:")
-  time.sleep(0.5)
-  print(random_roll)
-  time.sleep(1)
-  print("Your complete roll is:")
-  time.sleep(0.5)
-  print(sum)
+def roll_and_print(die_size, dice_modifer):
+  random_roll = random.randint(1, die_size)
+  result_roll = random_roll + dice_modifer
+  print("Your true roll is: %d" % random_roll)
+  print("Your complete roll is: {0}".format(result_roll))
 
-  roll()
+def main():
+  try:
+    size = int(input("What is the size of this dice?: "))
+    modifier = int(input("What is your modifier for this roll?: "))
+    roll_and_print(size, modifier)
+  except:
+    print("Please enter a valid number ty")
+    return
 
-roll()
+while True:
+  main()
